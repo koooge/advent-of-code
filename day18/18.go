8f07e9d1b686ea28bcd8538b7739cd3bc88bd308
koooge <koooooge@gmail.com>
2021-12-22 01:27:55 +0100

    day18: wip
    Signed-off-by: koooge <koooooge@gmail.com>

--- day18/18.go
+++ day18/18.go
@@ -37,19 +37,20 @@ func main() {
 
 	node := list[0]
 	for i := 1; i < len(list); i++ {
-		node := &Node{
-			Parent:   nil,
-			Left:     node,
-			Right:    list[i],
-			LeftNum:  -1,
-			RightNum: -1,
+		node = &Node{
+			Parent: nil,
+			Left:   node,
+			Right:  list[i],
 		}
 		node.Left.Parent = node
 		node.Right.Parent = node
 		node = reduce(node)
 	}
 
-	fmt.Printf("%+v", node)
+	fmt.Println("---")
+
+	result := magnitude(node)
+	fmt.Println(result)
 }
 
 func parseList(in string, parent *Node) *Node {
@@ -69,7 +70,6 @@ func parseList(in string, parent *Node) *Node {
 			a1, _ := strconv.Atoi(string(in[1+2]))
 			node.RightNum = a1
 		}
-
 	} else {
 		a0, _ := strconv.Atoi(string(in[1]))
 		node.LeftNum = a0
@@ -80,7 +80,6 @@ func parseList(in string, parent *Node) *Node {
 			node.RightNum = a1
 		}
 	}
-
 	return node
 }
 
@@ -105,25 +104,26 @@ func reduce(node *Node) *Node {
 	n := findExplode(node, 0)
 	if n != nil {
 		node.explode(n)
+		node.split()
 		node = reduce(node)
 	}
-	node.split()
 
 	return node
 }
 
 func findExplode(node *Node, d int) *Node {
-	if d == 4 {
+	if d >= 4 && node.Left == nil && node.Right == nil {
 		return node
 	}
 
+	var n *Node
 	if node.Left != nil {
-		return findExplode(node.Left, d+1)
+		n = findExplode(node.Left, d+1)
 	}
-	if node.Right != nil {
-		return findExplode(node.Right, d+1)
+	if n == nil && node.Right != nil {
+		n = findExplode(node.Right, d+1)
 	}
-	return nil
+	return n
 }
 
 func (node *Node) explode(n *Node) {
@@ -143,10 +143,18 @@ func addToLeft(node *Node, n int) {
 		if node.Parent.Left == nil {
 			node.Parent.LeftNum += n
 		} else if node.Parent.Left != node {
-			node.Parent.Left.RightNum += n
+			addToLeftRight(node.Parent.Left, n)
 		} else {
 			addToLeft(node.Parent, n)
 		}
+	}
+}
+
+func addToLeftRight(node *Node, n int) {
+	if node.Right == nil {
+		node.RightNum += n
+	} else {
+		addToLeftRight(node.Right, n)
 	}
 }
 
@@ -155,13 +163,58 @@ func addToRight(node *Node, n int) {
 		if node.Parent.Right == nil {
 			node.Parent.RightNum += n
 		} else if node.Parent.Right != node {
-			node.Parent.Right.LeftNum += n
+			addToRightLeft(node.Parent.Right, n)
 		} else {
 			addToRight(node.Parent, n)
 		}
 	}
 }
 
-func (node *Node) split() {
+func addToRightLeft(node *Node, n int) {
+	if node.Left == nil {
+		node.LeftNum += n
+	} else {
+		addToRightLeft(node.Left, n)
+	}
+}
 
+func (node *Node) split() {
+	if node.Left != nil {
+		node.Left.split()
+	}
+	if node.Right != nil {
+		node.Right.split()
+	}
+
+	if node.LeftNum >= 10 {
+		node.Left = &Node{
+			Parent:   node,
+			LeftNum:  node.LeftNum / 2,
+			RightNum: node.LeftNum - (node.LeftNum / 2),
+		}
+		node.LeftNum = 0
+	}
+
+	if node.RightNum >= 10 {
+		node.Right = &Node{
+			Parent:   node,
+			LeftNum:  node.RightNum / 2,
+			RightNum: node.RightNum - (node.RightNum / 2),
+		}
+		node.RightNum = 0
+	}
+}
+
+func magnitude(node *Node) int {
+	fmt.Printf("%+v\n", node)
+	res := 3*node.LeftNum + 2*node.RightNum
+
+	if node.Left != nil {
+		res += 3 * magnitude(node.Left)
+	}
+	if node.Right != nil {
+		res += 2 * magnitude(node.Right)
+	}
+
+	return res
 }
