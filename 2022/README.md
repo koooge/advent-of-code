3097574f0cffd8d256b3760c976de2e5ff7668c6
koooge <koooooge@gmail.com>
2022-12-02 00:34:44 +0100

    day01: :star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/README.md
+++ 2022/README.md
@@ -3,7 +3,15 @@
 Let's go rust
 
 ```sh
+# create project
 $ cargo new --lib day01
 $ cd day01
+
+# Edit src/lib.rs
+
+# test
 $ cargo test
+
+# test with println!
+$ cargo test -- --nocapture
 ```
--- 2022/day01/src/lib.rs
+++ 2022/day01/src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn task1(inputs: Vec<String>) -> u32 {
+  let mut ret: u32 = 0;
+  let mut calories: u32 = 0;
+
+  for input in inputs {
+    if input.len() == 0 {
+      ret = if calories > ret { calories} else { ret };
+      calories = 0;
+      continue;
+    }
+
+    let calory: u32 = input.parse().unwrap();
+    calories += calory;
+  }
+
+  ret
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+      let inputs = read_file("./src/test1.txt");
+      let result = task1(inputs);
+      println!("{}", result);
+      assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn input1() {
+      let inputs = read_file("./src/input1.txt");
+      let result = task1(inputs);
+      println!("{}", result);
+      assert_eq!(result, 71934);
+    }
+
+    fn read_file(file_path: &str) -> Vec<String> {
+      let contents = fs::read_to_string(file_path);
+      let mut ret: Vec<String> = vec![];
+      match contents {
+        Ok(contents) => {
+          for line in contents.lines() {
+            ret.push(line.trim().to_string());
+          }
+        },
+        Err(why) => eprintln!("{}", why),
+      }
+      ret
     }
 }
