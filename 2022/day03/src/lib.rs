9ba11276069bc3b2cef07668c8b9d3e09f1ebf39
koooge <koooooge@gmail.com>
2022-12-03 18:29:17 +0100

    day03: :star::star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day03/src/lib.rs
+++ 2022/day03/src/lib.rs
@@ -18,6 +18,35 @@ pub fn solve_part1(inputs: Vec<String>) -> u32 {
     ret
 }
 
+pub fn solve_part2(inputs: Vec<String>) -> u32 {
+    let priorities = String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    let mut ret: u32 = 0;
+    let ngroup = inputs.len() / 3;
+
+    for i in 0..ngroup {
+        let badge: char = find_badge(&inputs.get((i * 3)..=(i * 3) + 2).unwrap().to_vec());
+        let prio = priorities.find(badge).unwrap() as u32 + 1;
+        ret += prio;
+    }
+
+    ret
+}
+
+fn find_badge(inputs: &Vec<String>) -> char {
+    let mut badges: Vec<char> = inputs[0].clone().chars().collect();
+    badges.sort();
+    badges.dedup();
+
+    for input in inputs[1..=2].iter() {
+        let mut line: Vec<char> = input.clone().chars().collect();
+        line.sort();
+        line.dedup();
+        badges.retain(|&x| line.contains(&x));
+    }
+
+    *badges.get(0).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,21 +68,21 @@ mod tests {
         assert_eq!(result, 7967);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 12);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, 70);
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 9975);
-    // }
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, 58);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
