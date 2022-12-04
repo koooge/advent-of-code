dae7278725458ec3e8eb7a13d0577ac2bab133d3
koooge <koooooge@gmail.com>
2022-12-04 18:27:46 +0100

    day04: :star::star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day04/src/lib.rs
+++ 2022/day04/src/lib.rs
@@ -2,12 +2,50 @@ pub fn solve_part1(inputs: Vec<String>) -> u32 {
     let mut ret: u32 = 0;
 
     for input in inputs {
-        let pairs: Vec<&str> = input.split(',').collect(); // map(|x| x.split('-')).collect();
-        let first: Vec<u32> = pairs.get(0).unwrap().split('-').map(|x| x.parse().unwrap()).collect();
-        let second: Vec<u32> = pairs.get(1).unwrap().split('-').map(|x| x.parse().unwrap()).collect();
+        let pairs: Vec<&str> = input.split(',').collect();
+        let first: Vec<u32> = pairs
+            .get(0)
+            .unwrap()
+            .split('-')
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let second: Vec<u32> = pairs
+            .get(1)
+            .unwrap()
+            .split('-')
+            .map(|x| x.parse().unwrap())
+            .collect();
 
         if (first[0] <= second[0] && second[1] <= first[1])
             || (second[0] <= first[0] && first[1] <= second[1])
+        {
+            ret += 1;
+        }
+    }
+
+    ret
+}
+
+pub fn solve_part2(inputs: Vec<String>) -> u32 {
+    let mut ret: u32 = 0;
+
+    for input in inputs {
+        let pairs: Vec<&str> = input.split(',').collect();
+        let first: Vec<u32> = pairs
+            .get(0)
+            .unwrap()
+            .split('-')
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let second: Vec<u32> = pairs
+            .get(1)
+            .unwrap()
+            .split('-')
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        if (first[0] <= second[0] && second[0] <= first[1])
+            || (second[0] <= first[0] && first[0] <= second[1])
         {
             ret += 1;
         }
@@ -37,21 +75,21 @@ mod tests {
         assert_eq!(result, 483);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 70);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, 4);
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 58);
-    // }
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, 874);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
