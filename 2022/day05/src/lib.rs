c1040516667356f6fa7a6c906d983dd59cd028fe
koooge <koooooge@gmail.com>
2022-12-08 13:07:49 +0900

    day05: :star::star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day05/src/lib.rs
+++ 2022/day05/src/lib.rs
@@ -1,30 +1,69 @@
 pub fn solve_part1(inputs: Vec<String>) -> String {
-  let mut stacks: Vec<Vec<char>> = Vec::new();
+    let mut stacks: Vec<Vec<char>> = Vec::new();
 
-  for input in inputs {
-    let line = input.trim();
+    for input in inputs {
+        let line = input.trim();
 
-    if line.starts_with('[') {
-      for (i, c) in input.chars().enumerate() {
-        if c == '[' || c == ']' || c == ' ' { continue; }
+        if line.starts_with('[') {
+            for (i, c) in input.chars().enumerate() {
+                if c == '[' || c == ']' || c == ' ' {
+                    continue;
+                }
 
-        let index = (i -1) / 4;
-        while stacks.len() <= index { stacks.push(vec![]); }
-        stacks[index].insert(0, c);
-      }
-    } else if line.starts_with("move") {
-      let ops: Vec<&str> = input.split(' ').collect();
-      let n: usize = ops[1].parse().unwrap();
-      let from: usize = ops[3].parse().unwrap();
-      let to: usize = ops[5].parse().unwrap();
-      for _ in 0..n {
-        let c = stacks[from - 1].pop().unwrap();
-        stacks[to - 1].push(c);
-      }
+                let index = (i - 1) / 4;
+                while stacks.len() <= index {
+                    stacks.push(vec![]);
+                }
+                stacks[index].insert(0, c);
+            }
+        } else if line.starts_with("move") {
+            let ops: Vec<&str> = input.split(' ').collect();
+            let n: usize = ops[1].parse().unwrap();
+            let from: usize = ops[3].parse().unwrap();
+            let to: usize = ops[5].parse().unwrap();
+            for _ in 0..n {
+                let c = stacks[from - 1].pop().unwrap();
+                stacks[to - 1].push(c);
+            }
+        }
     }
-  }
 
-  stacks.iter().map(|v| v.last().unwrap()).collect()
+    stacks.iter().map(|v| v.last().unwrap()).collect()
+}
+
+pub fn solve_part2(inputs: Vec<String>) -> String {
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+
+    for input in inputs {
+        let line = input.trim();
+
+        if line.starts_with('[') {
+            for (i, c) in input.chars().enumerate() {
+                if c == '[' || c == ']' || c == ' ' {
+                    continue;
+                }
+
+                let index = (i - 1) / 4;
+                while stacks.len() <= index {
+                    stacks.push(vec![]);
+                }
+                stacks[index].insert(0, c);
+            }
+        } else if line.starts_with("move") {
+            let ops: Vec<&str> = input.split(' ').collect();
+            let n: usize = ops[1].parse().unwrap();
+            let from: usize = ops[3].parse().unwrap();
+            let to: usize = ops[5].parse().unwrap();
+            let mut moving: Vec<char> = Vec::new();
+            for _ in 0..n {
+                moving.push(stacks[from - 1].pop().unwrap());
+            }
+            moving.reverse();
+            stacks[to - 1].append(&mut moving);
+        }
+    }
+
+    stacks.iter().map(|v| v.last().unwrap()).collect()
 }
 
 #[cfg(test)]
@@ -48,21 +87,21 @@ mod tests {
         assert_eq!(result, "TLFGBZHCN");
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 4);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, "MCD");
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 874);
-    // }
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, "QRQFHFWCL");
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
