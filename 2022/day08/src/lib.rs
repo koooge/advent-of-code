9402dea7f7865d850fe3cc2a60e7761fcc9d2311
koooge <koooooge@gmail.com>
2022-12-10 11:54:32 +0900

    day08: :star::star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day08/src/lib.rs
+++ 2022/day08/src/lib.rs
@@ -54,6 +54,71 @@ pub fn solve_part1(inputs: Vec<String>) -> u32 {
     ret
 }
 
+use std::cmp;
+
+fn calc_score(r: usize, c: usize, inputs: &Vec<String>) -> u32 {
+    let row: Vec<u32> = inputs[r].chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let col: Vec<u32> = inputs
+        .iter()
+        .map(|r| r.chars().nth(c).unwrap().to_digit(10).unwrap())
+        .collect();
+
+    let mut base: u32 = row[c];
+    let mut left: u32 = 0;
+    for i in (0..c).rev() {
+        left += 1;
+        if row[i] >= base {
+            break;
+        }
+        base = cmp::max(row[i], base);
+    }
+
+    base = row[c];
+    let mut right: u32 = 0;
+    for i in (c + 1)..row.len() {
+        right += 1;
+        if row[i] >= row[c] {
+            break;
+        }
+        base = cmp::max(row[i], base);
+    }
+
+    base = row[c];
+    let mut top: u32 = 0;
+    for i in (0..r).rev() {
+        top += 1;
+        if col[i] >= row[c] {
+            break;
+        }
+        base = cmp::max(col[i], base);
+    }
+
+    base = row[c];
+    let mut bottom: u32 = 0;
+    for i in (r + 1)..inputs.len() {
+        bottom += 1;
+        if col[i] >= base {
+            break;
+        }
+        base = cmp::max(col[i], base);
+    }
+
+    top * right * bottom * left
+}
+
+pub fn solve_part2(inputs: Vec<String>) -> u32 {
+    let mut max_score: u32 = 0;
+
+    for i in 1..(inputs.len() - 1) {
+        for j in 1..(inputs[i].len() - 1) {
+            let score = calc_score(i, j, &inputs);
+            max_score = cmp::max(score, max_score);
+        }
+    }
+
+    max_score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,21 +140,21 @@ mod tests {
         assert_eq!(result, 1798);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 19);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, 8);
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 3495);
-    // }
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(inputs);
+        println!("{}", result);
+        assert_eq!(result, 259308);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
