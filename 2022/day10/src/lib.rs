61a066e3a10d21a384953b823429abadf5f389b2
koooge <koooooge@gmail.com>
2022-12-11 14:53:27 +0900

    day10: :star::star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day10/src/lib.rs
+++ 2022/day10/src/lib.rs
@@ -28,6 +28,34 @@ pub fn solve_part1(inputs: Vec<String>) -> i32 {
     ret
 }
 
+fn get_pixel(cycle: u32, sprite: i32) -> bool {
+    let col: i32 = i32::try_from(cycle).unwrap() % 40;
+    [sprite - 1, sprite, sprite + 1].contains(&col)
+}
+
+pub fn solve_part2(inputs: Vec<String>) -> [bool; 240] {
+    let mut cycle: u32 = 0;
+    let mut reg_v: i32 = 1; // 1 to 38
+    let mut crt: [bool; 240] = [false; 240];
+
+    for input in inputs {
+        let ins: Vec<&str> = input.split(' ').collect();
+        let opcode: &str = ins.get(0).unwrap();
+
+        crt[cycle as usize] = get_pixel(cycle, reg_v);
+        cycle += 1;
+
+        if opcode == "addx" {
+            crt[cycle as usize] = get_pixel(cycle, reg_v);
+            cycle += 1;
+            let x: i32 = ins.get(1).unwrap().parse().unwrap();
+            reg_v += x;
+        }
+    }
+
+    crt
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,21 +77,82 @@ mod tests {
         assert_eq!(result, 13760);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test2.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 36);
-    // }
+    fn print_crt(crt: [bool; 240]) {
+        for i in 0..240 {
+            if crt[i] {
+                print!("#");
+            } else {
+                print!(".");
+            }
+            if (i + 1) % 40 == 0 {
+                println!("");
+            }
+        }
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(inputs);
-    //     println!("{}", result);
-    //     assert_eq!(result, 2522);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(inputs);
+        print_crt(result);
+        assert_eq!(
+            result,
+            [
+                true, true, false, false, true, true, false, false, true, true, false, false, true,
+                true, false, false, true, true, false, false, true, true, false, false, true, true,
+                false, false, true, true, false, false, true, true, false, false, true, true,
+                false, false, true, true, true, false, false, false, true, true, true, false,
+                false, false, true, true, true, false, false, false, true, true, true, false,
+                false, false, true, true, true, false, false, false, true, true, true, false,
+                false, false, true, true, true, false, true, true, true, true, false, false, false,
+                false, true, true, true, true, false, false, false, false, true, true, true, true,
+                false, false, false, false, true, true, true, true, false, false, false, false,
+                true, true, true, true, false, false, false, false, true, true, true, true, true,
+                false, false, false, false, false, true, true, true, true, true, false, false,
+                false, false, false, true, true, true, true, true, false, false, false, false,
+                false, true, true, true, true, true, false, false, false, false, false, true, true,
+                true, true, true, true, false, false, false, false, false, false, true, true, true,
+                true, true, true, false, false, false, false, false, false, true, true, true, true,
+                true, true, false, false, false, false, false, false, true, true, true, true, true,
+                true, true, true, true, true, true, false, false, false, false, false, false,
+                false, true, true, true, true, true, true, true, false, false, false, false, false,
+                false, false, true, true, true, true, true, true, true, false, false, false, false,
+                false
+            ]
+        );
+    }
+
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(inputs);
+        print_crt(result);
+        assert_eq!(
+            result,
+            [
+                true, true, true, false, false, true, true, true, true, false, true, false, false,
+                true, false, true, true, true, true, false, false, true, true, false, false, true,
+                true, true, false, false, true, true, true, true, false, true, true, true, true,
+                false, true, false, false, true, false, true, false, false, false, false, true,
+                false, true, false, false, false, false, false, true, false, true, false, false,
+                true, false, true, false, false, true, false, true, false, false, false, false,
+                true, false, false, false, false, true, false, false, true, false, true, true,
+                true, false, false, true, true, false, false, false, false, false, true, false,
+                false, true, false, false, false, false, true, false, false, true, false, true,
+                true, true, false, false, true, true, true, false, false, true, true, true, false,
+                false, true, false, false, false, false, true, false, true, false, false, false,
+                true, false, false, false, true, false, false, false, false, true, true, true,
+                false, false, true, false, false, false, false, true, false, false, false, false,
+                true, false, true, false, false, true, false, false, false, false, true, false,
+                true, false, false, true, false, false, false, false, true, false, false, true,
+                false, true, false, false, false, false, true, false, false, false, false, true,
+                false, false, false, false, true, false, false, true, false, true, false, false,
+                false, false, true, false, false, true, false, true, true, true, true, false,
+                false, true, true, false, false, true, false, false, false, false, true, true,
+                true, true, false, true, false, false, false, false
+            ]
+        );
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
