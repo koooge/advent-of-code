900be955dcaeebea14c380bfd44e041a6dee8c47
koooge <koooooge@gmail.com>
2022-12-13 16:19:56 +0900

    day12: :star::star:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day12/src/lib.rs
+++ 2022/day12/src/lib.rs
@@ -74,6 +74,86 @@ pub fn solve_part1(inputs: Vec<String>) -> u32 {
     steps[pos_e.0][pos_e.1] as u32
 }
 
+fn map_steps_rev(
+    heights: &[Vec<char>],
+    steps: &mut [Vec<u32>],
+    y_len: usize,
+    x_len: usize,
+    y: usize,
+    x: usize,
+) {
+    let yxu8: u8 = heights[y][x] as u8;
+    if y > 0
+        && steps[y - 1][x] > steps[y][x] + 1
+        && ((heights[y][x] >= 'a' && heights[y - 1][x] as u8 >= yxu8 - 1)
+            || heights[y][x] == 'E' && heights[y - 1][x] == 'z'
+            || heights[y][x] == 'a' && heights[y - 1][x] == 'S')
+    {
+        steps[y - 1][x] = steps[y][x] + 1;
+        map_steps_rev(heights, steps, y_len, x_len, y - 1, x);
+    }
+
+    if x + 1 < x_len
+        && steps[y][x + 1] > steps[y][x] + 1
+        && ((heights[y][x] >= 'a' && heights[y][x + 1] as u8 >= yxu8 - 1)
+            || heights[y][x] == 'E' && heights[y][x + 1] == 'z'
+            || heights[y][x] == 'a' && heights[y][x + 1] == 'S')
+    {
+        steps[y][x + 1] = steps[y][x] + 1;
+        map_steps_rev(heights, steps, y_len, x_len, y, x + 1);
+    }
+
+    if y + 1 < y_len
+        && steps[y + 1][x] > steps[y][x] + 1
+        && ((heights[y][x] >= 'a' && heights[y + 1][x] as u8 >= yxu8 - 1)
+            || heights[y][x] == 'E' && heights[y + 1][x] == 'z'
+            || heights[y][x] == 'a' && heights[y + 1][x] == 'S')
+    {
+        steps[y + 1][x] = steps[y][x] + 1;
+        map_steps_rev(heights, steps, y_len, x_len, y + 1, x);
+    }
+
+    if x > 0
+        && steps[y][x - 1] > steps[y][x] + 1
+        && ((heights[y][x] >= 'a' && heights[y][x - 1] as u8 >= yxu8 - 1)
+            || heights[y][x] == 'E' && heights[y][x - 1] == 'z'
+            || heights[y][x] == 'a' && heights[y][x - 1] == 'S')
+    {
+        steps[y][x - 1] = steps[y][x] + 1;
+        map_steps_rev(heights, steps, y_len, x_len, y, x - 1);
+    }
+}
+
+pub fn solve_part2(inputs: Vec<String>) -> u32 {
+    let mut heights: Vec<Vec<char>> = Vec::new();
+    let mut pos_e: (usize, usize) = (0, 0);
+
+    for (i, input) in inputs.iter().enumerate() {
+        let line: Vec<char> = input.chars().collect();
+        if let Some(j) = line.iter().position(|&x| x == 'E') {
+            pos_e = (i, j);
+        }
+        heights.push(line);
+    }
+    let map_size: (usize, usize) = (heights.len(), heights[0].len());
+    let mut steps: Vec<Vec<u32>> = vec![vec![u32::MAX; map_size.1]; map_size.0];
+    steps[pos_e.0][pos_e.1] = 0;
+
+    map_steps_rev(
+        &heights, &mut steps, map_size.0, map_size.1, pos_e.0, pos_e.1,
+    );
+
+    let mut min: u32 = u32::MAX;
+    for i in 0..heights.len() {
+        for j in 0..heights[i].len() {
+            if heights[i][j] == 'a' {
+                min = if steps[i][j] < min { steps[i][j] } else { min };
+            }
+        }
+    }
+    min
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -95,69 +175,19 @@ mod tests {
         assert_eq!(result, 361);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test1.txt");
-    //     let result = solve_part2(inputs);
-    //     print_crt(result);
-    //     assert_eq!(
-    //         result,
-    //         [
-    //             true, true, false, false, true, true, false, false, true, true, false, false, true,
-    //             true, false, false, true, true, false, false, true, true, false, false, true, true,
-    //             false, false, true, true, false, false, true, true, false, false, true, true,
-    //             false, false, true, true, true, false, false, false, true, true, true, false,
-    //             false, false, true, true, true, false, false, false, true, true, true, false,
-    //             false, false, true, true, true, false, false, false, true, true, true, false,
-    //             false, false, true, true, true, false, true, true, true, true, false, false, false,
-    //             false, true, true, true, true, false, false, false, false, true, true, true, true,
-    //             false, false, false, false, true, true, true, true, false, false, false, false,
-    //             true, true, true, true, false, false, false, false, true, true, true, true, true,
-    //             false, false, false, false, false, true, true, true, true, true, false, false,
-    //             false, false, false, true, true, true, true, true, false, false, false, false,
-    //             false, true, true, true, true, true, false, false, false, false, false, true, true,
-    //             true, true, true, true, false, false, false, false, false, false, true, true, true,
-    //             true, true, true, false, false, false, false, false, false, true, true, true, true,
-    //             true, true, false, false, false, false, false, false, true, true, true, true, true,
-    //             true, true, true, true, true, true, false, false, false, false, false, false,
-    //             false, true, true, true, true, true, true, true, false, false, false, false, false,
-    //             false, false, true, true, true, true, true, true, true, false, false, false, false,
-    //             false
-    //         ]
-    //     );
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(inputs);
+        assert_eq!(result, 29);
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(inputs);
-    //     print_crt(result);
-    //     assert_eq!(
-    //         result,
-    //         [
-    //             true, true, true, false, false, true, true, true, true, false, true, false, false,
-    //             true, false, true, true, true, true, false, false, true, true, false, false, true,
-    //             true, true, false, false, true, true, true, true, false, true, true, true, true,
-    //             false, true, false, false, true, false, true, false, false, false, false, true,
-    //             false, true, false, false, false, false, false, true, false, true, false, false,
-    //             true, false, true, false, false, true, false, true, false, false, false, false,
-    //             true, false, false, false, false, true, false, false, true, false, true, true,
-    //             true, false, false, true, true, false, false, false, false, false, true, false,
-    //             false, true, false, false, false, false, true, false, false, true, false, true,
-    //             true, true, false, false, true, true, true, false, false, true, true, true, false,
-    //             false, true, false, false, false, false, true, false, true, false, false, false,
-    //             true, false, false, false, true, false, false, false, false, true, true, true,
-    //             false, false, true, false, false, false, false, true, false, false, false, false,
-    //             true, false, true, false, false, true, false, false, false, false, true, false,
-    //             true, false, false, true, false, false, false, false, true, false, false, true,
-    //             false, true, false, false, false, false, true, false, false, false, false, true,
-    //             false, false, false, false, true, false, false, true, false, true, false, false,
-    //             false, false, true, false, false, true, false, true, true, true, true, false,
-    //             false, true, true, false, false, true, false, false, false, false, true, true,
-    //             true, true, false, true, false, false, false, false
-    //         ]
-    //     );
-    // }
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(inputs);
+        assert_eq!(result, 354);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
