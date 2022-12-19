ebb9dc0ceb30a8c0703befa3e45fd749a6c0e02c
koooge <koooooge@gmail.com>
2022-12-19 18:43:47 +0900

    day19: :star: wrong algorithm
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2022/day19/src/lib.rs
+++ 2022/day19/src/lib.rs
@@ -8,19 +8,18 @@ struct Blueprint {
 }
 
 fn tick_minute(blueprint: &Blueprint, minute: u32, resources: [u32; 4], robots: [u32; 4]) -> u32 {
-    if minute == 24 {
+    if minute == 0 {
         return resources[3];
     }
 
     let mut max_geode: u32 = 0;
-    let mut does_spent_ore: bool = false;
 
     // geode
     if resources[0] >= blueprint.geode.0 && resources[2] >= blueprint.geode.1 {
         return cmp::max(
             tick_minute(
                 blueprint,
-                minute + 1,
+                minute - 1,
                 [
                     resources[0] + robots[0] - blueprint.geode.0,
                     resources[1] + robots[1],
@@ -38,7 +37,7 @@ fn tick_minute(blueprint: &Blueprint, minute: u32, resources: [u32; 4], robots: 
         return cmp::max(
             tick_minute(
                 blueprint,
-                minute + 1,
+                minute - 1,
                 [
                     resources[0] + robots[0] - blueprint.obsidian.0,
                     resources[1] + robots[1] - blueprint.obsidian.1,
@@ -56,7 +55,7 @@ fn tick_minute(blueprint: &Blueprint, minute: u32, resources: [u32; 4], robots: 
         max_geode = cmp::max(
             tick_minute(
                 blueprint,
-                minute + 1,
+                minute - 1,
                 [
                     resources[0] + robots[0] - blueprint.clay,
                     resources[1] + robots[1],
@@ -74,7 +73,7 @@ fn tick_minute(blueprint: &Blueprint, minute: u32, resources: [u32; 4], robots: 
         max_geode = cmp::max(
             tick_minute(
                 blueprint,
-                minute + 1,
+                minute - 1,
                 [
                     resources[0] + robots[0] - blueprint.ore,
                     resources[1] + robots[1],
@@ -90,7 +89,7 @@ fn tick_minute(blueprint: &Blueprint, minute: u32, resources: [u32; 4], robots: 
     max_geode = cmp::max(
         tick_minute(
             blueprint,
-            minute + 1,
+            minute - 1,
             [
                 resources[0] + robots[0],
                 resources[1] + robots[1],
@@ -131,7 +130,41 @@ pub fn solve_part1(inputs: &[String]) -> u32 {
             geode: (geode_ore, geode_obsidian),
         };
 
-        let max_geode = tick_minute(&blueprint, 0, [0; 4], [1, 0, 0, 0]);
+        let max_geode = tick_minute(&blueprint, 24, [0; 4], [1, 0, 0, 0]);
+        ret = ret + (level * max_geode);
+    }
+
+    ret
+}
+
+pub fn solve_part2(inputs: &[String]) -> u32 {
+    let mut ret: u32 = 0;
+    let mut level: u32 = 0;
+
+    for input in inputs {
+        level += 1;
+        let line: Vec<&str> = input.split('.').collect();
+        let ore: u32 = line[0].split(' ').collect::<Vec<&str>>()[6]
+            .parse()
+            .unwrap();
+        let clay: u32 = line[1].split(' ').collect::<Vec<&str>>()[5]
+            .parse()
+            .unwrap();
+        let obsidian: Vec<&str> = line[2].split(' ').collect();
+        let obsidian_ore: u32 = obsidian[5].parse().unwrap();
+        let obsidian_clay: u32 = obsidian[8].parse().unwrap();
+        let geode: Vec<&str> = line[3].split(' ').collect();
+        let geode_ore: u32 = geode[5].parse().unwrap();
+        let geode_obsidian: u32 = geode[8].parse().unwrap();
+        let blueprint: Blueprint = Blueprint {
+            ore,
+            clay,
+            obsidian: (obsidian_ore, obsidian_clay),
+            geode: (geode_ore, geode_obsidian),
+        };
+
+        let max_geode = tick_minute(&blueprint, 32, [0; 4], [1, 0, 0, 0]);
+        println!("{}", max_geode);
         ret = ret + (level * max_geode);
     }
 
@@ -158,19 +191,19 @@ mod tests {
         assert_eq!(result, 1624);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/test1.txt");
-    //     let result = solve_part2(&inputs);
-    //     assert_eq!(result, 58);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/test1.txt");
+        let result = solve_part2(&inputs[0..2]);
+        assert_eq!(result, 62);
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/input1.txt");
-    //     let result = solve_part2(&inputs);
-    //     assert_eq!(result, 2018);
-    // }
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/input1.txt");
+        let result = solve_part2(&inputs[0..3]);
+        assert_eq!(result, 62);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
         let contents = fs::read_to_string(file_path);
