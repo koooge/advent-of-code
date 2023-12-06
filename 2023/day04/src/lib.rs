b93978385e640c6ad8886fb20beafb36fda953f8
koooge <koooooge@gmail.com>
2023-12-06 17:38:40 +0100

    2023/day04: ⭐⭐
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2023/day04/src/lib.rs
+++ 2023/day04/src/lib.rs
@@ -17,11 +17,27 @@ pub fn solve_part1(inputs: &[String]) -> usize {
   ret
 }
 
-// pub fn solve_part2(inputs: &[String]) -> usize {
-//   let mut ret = 0;
+pub fn solve_part2(inputs: &[String]) -> usize {
+  let mut ret: Vec<usize> = vec![1; inputs.len()];
 
-//   ret
-// }
+  for (i, input) in inputs.iter().enumerate() {
+    let mut winning_num = 0;
+    let nums = input.split(": ").last().unwrap();
+    let left = nums.split(" | ").next().unwrap().split_whitespace().collect::<Vec<&str>>();
+    let right = nums.split(" | ").last().unwrap().split_whitespace().collect::<Vec<&str>>();
+    for n in left {
+      if right.iter().find(|&&x| x == n) != None {
+        winning_num += 1;
+      }
+    }
+
+    for j in 1..=winning_num {
+      ret[i+j] += ret[i];
+    }
+  }
+
+  ret.iter().sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -42,19 +58,19 @@ mod tests {
       assert_eq!(result, 26443);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //   let inputs = read_file("./src/test1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 467835);
-    // }
+    #[test]
+    fn part2_case1() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs);
+      assert_eq!(result, 30);
+    }
 
-    // #[test]
-    // fn part2() {
-    //   let inputs = read_file("./src/input1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 81997870);
-    // }
+    #[test]
+    fn part2() {
+      let inputs = read_file("./src/input1.txt");
+      let result = solve_part2(&inputs);
+      assert_eq!(result, 6284877);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
       let contents = fs::read_to_string(file_path);
