440f709be278210287705559bcfd465438cbd0db
koooge <koooooge@gmail.com>
2023-12-17 18:01:19 +0100

    2023/day13: ⭐❓
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2023/day13/src/lib.rs
+++ 2023/day13/src/lib.rs
@@ -69,9 +69,115 @@ pub fn solve_part1(inputs: &[String]) -> usize {
   ret.iter().sum()
 }
 
-// pub fn solve_part2(inputs: &[String], expand_scale: usize) -> usize {
-//   0
-// }
+fn has_horizontal_line(pattern: &[Vec<char>]) -> bool {
+  let mut is_horizontal = true;
+  for i in 0..pattern.len()-1 {
+    if pattern[i].iter().collect::<String>() == pattern[i+1].iter().collect::<String>() {
+      for j in (0..i).rev() {
+        if pattern.get(i+1+(i-j)) == None {
+          break;
+        }
+        if pattern[j].iter().collect::<String>() != pattern[i+1+(i-j)].iter().collect::<String>() {
+          is_horizontal = false;
+          break;
+        }
+      }
+    }
+  }
+  is_horizontal
+}
+
+fn diff(a: &[char], b: &[char]) -> usize {
+  let mut ret = 0;
+  for i in 0..a.len() {
+    if a[i] != b[i] {
+      ret += 1;
+    }
+  }
+  ret
+}
+
+pub fn solve_part2(inputs: &[String]) -> usize {
+  let mut ret: Vec<usize> = vec![];
+  let mut patterns: Vec<Vec<Vec<char>>> = vec![];
+
+  // parse
+  let mut patterns_i = 0;
+  for input in inputs {
+    if input != "" {
+      let line: Vec<char> = input.chars().collect();
+      if patterns.get(patterns_i) == None {
+        patterns.push(vec![]);
+      }
+      patterns[patterns_i].push(line);
+    } else {
+      patterns_i += 1;
+    }
+  }
+
+  for pattern in patterns {
+    let horizontal = has_horizontal_line(&pattern);
+    if !horizontal {
+      for i in 0..pattern.len()-1 {
+        let mut is_horizontal = true;
+        if pattern[i].iter().collect::<String>() == pattern[i+1].iter().collect::<String>() {
+          let mut smudge = 0;
+          for j in (0..i).rev() {
+            if pattern.get(i+1+(i-j)) == None {
+              break;
+            }
+            let d = diff(&pattern[j], &pattern[i+1+(i-j)]);
+            if d == 1 && smudge == 0 {
+              smudge = 1;
+            } else if d > 0 {
+              is_horizontal = false;
+              break;
+            }
+          }
+          if is_horizontal {
+            ret.push((i+1) * 100);
+          }
+        }
+      }
+    } else {
+      for i in 0..pattern[0].len()-1 {
+        let mut is_vertical = true;
+        let mut a: Vec<char> = vec![];
+        let mut b: Vec<char> = vec![];
+        for j in 0..pattern.len() {
+          a.push(pattern[j][i]);
+          b.push(pattern[j][i+1]);
+        }
+        if a.iter().collect::<String>() == b.iter().collect::<String>() {
+          let mut smudge = 0;
+          for j in (0..i).rev() {
+            if pattern[0].get(i+1+(i-j)) == None {
+              break;
+            }
+            let mut a: Vec<char> = vec![];
+            let mut b: Vec<char> = vec![];
+            for k in 0..pattern.len() {
+              a.push(pattern[k][j]);
+              b.push(pattern[k][i+1+(i-j)]);
+            }
+            let d = diff(&a, &b);
+            if d == 1 && smudge == 0 {
+              smudge = 1;
+            } else if d > 0 {
+              is_vertical = false;
+              break;
+            }
+          }
+          if is_vertical {
+            ret.push(i+1);
+          }
+        }
+      }
+    }
+  }
+
+  ret.iter().sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -106,12 +212,12 @@ mod tests {
       assert_eq!(result, 42974);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //   let inputs = read_file("./src/test1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 1030);
-    // }
+    #[test]
+    fn part2_case1() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs);
+      assert_eq!(result, 400);
+    }
 
     // #[test]
     // fn part2() {
