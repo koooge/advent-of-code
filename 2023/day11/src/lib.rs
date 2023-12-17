755fea266c4b5e16da9b47eed4fa6e2b0fba9d76
koooge <koooooge@gmail.com>
2023-12-17 01:25:52 +0100

    2023/day11: 🐢
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2023/day11/src/lib.rs
+++ 2023/day11/src/lib.rs
@@ -48,11 +48,59 @@ pub fn solve_part1(inputs: &[String]) -> usize {
   ret
 }
 
-// pub fn solve_part2(inputs: &[String]) -> usize {
-//   let mut ret = 0;
+pub fn solve_part2(inputs: &[String], expand_scale: usize) -> usize {
+  let mut cosmic: Vec<Vec<char>> = inputs.to_vec().iter()
+    .map(|x| x.chars().collect::<Vec<char>>()).collect();
 
-//   ret
-// }
+  // cosmic expansion
+  for i in (0..cosmic.len()).rev() {
+    if !cosmic[i].contains(&'#') {
+      for k in 0..expand_scale-1 {
+        let space = cosmic[i].clone();
+        cosmic.insert(i + k, space);
+      }
+    }
+  }
+  for i in (0..cosmic[0].len()).rev() {
+    let mut nogalaxy = true;
+    for j in 0..cosmic.len() {
+      if cosmic[j][i] == '#' {
+        nogalaxy = false;
+        break
+      }
+    }
+    if nogalaxy {
+      for j in 0..cosmic.len() {
+        for k in 0..expand_scale-1 {
+          cosmic[j].insert(i + k, '.');
+        }
+      }
+    }
+  }
+
+  // find galaxy
+  let mut galaxies: Vec<(usize, usize)> = vec![];
+  for (i, row) in cosmic.iter().enumerate() {
+    for (j, c) in row.iter().enumerate() {
+      if c == &'#' {
+        galaxies.push((i, j));
+      }
+    }
+  }
+
+  // combinations
+  let mut ret = 0;
+  for (i, a) in galaxies.iter().enumerate() {
+    for j in i..galaxies.len() {
+      let b = galaxies.get(j).unwrap();
+      let x = if a.0 > b.0 { a.0 - b.0 } else { b.0 - a.0 };
+      let y = if a.1 > b.1 { a.1 - b.1 } else { b.1 - a.1 };
+      ret += x + y;
+    }
+  }
+
+  ret
+}
 
 #[cfg(test)]
 mod tests {
@@ -73,19 +121,26 @@ mod tests {
       assert_eq!(result, 9648398);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //   let inputs = read_file("./src/test1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 2);
-    // }
+    #[test]
+    fn part2_case1() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 10);
+      assert_eq!(result, 1030);
+    }
 
-    // #[test]
-    // fn part2() {
-    //   let inputs = read_file("./src/input1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 977);
-    // }
+    #[test]
+    fn part2_case2() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 100);
+      assert_eq!(result, 8410);
+    }
+
+    #[test]
+    fn part2() {
+      let inputs = read_file("./src/input1.txt");
+      let result = solve_part2(&inputs, 1_000_000);
+      assert_eq!(result, 0);
+    }
 
     fn read_file(file_path: &str) -> Vec<String> {
       let contents = fs::read_to_string(file_path);
