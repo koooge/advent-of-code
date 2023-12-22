079aac1ff50be40404b1de8031f5143ca0046060
koooge <koooooge@gmail.com>
2023-12-22 22:42:14 +0100

    2023/day21: ⭐🐢
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2023/day21/src/lib.rs
+++ 2023/day21/src/lib.rs
@@ -40,9 +40,50 @@ pub fn solve_part1(inputs: &[String], step: usize) -> usize {
   state.len()
 }
 
-// pub fn solve_part2(inputs: &[String]) -> usize {
-//   0
-// }
+pub fn solve_part2(inputs: &[String], step: usize) -> usize {
+  let mut map: Vec<Vec<char>> = vec![];
+  let mut s: (isize, isize) = (isize::MIN, isize::MIN);
+
+  for (i, input) in inputs.iter().enumerate() {
+    let row: Vec<char> = input.chars().collect();
+    let j = row.iter().position(|&c| c == 'S');
+    if j != None {
+      s = (i as isize, j.unwrap() as isize);
+    }
+    map.push(row);
+  }
+
+  let mut state: HashSet<(isize, isize)> = HashSet::from([s]);
+  for _ in 1..=step {
+    let mut next: HashSet<(isize, isize)> = HashSet::new();
+    for s in state {
+      let remainder: (isize, isize) = (s.0 % (map[0].len() as isize), s.1 % (map.len() as isize));
+      let pos: (usize, usize) = (
+        if remainder.0 >= 0 { remainder.0  as usize } else { ((map[0].len() as isize) + remainder.0) as usize },
+        if remainder.1 >= 0 { remainder.1  as usize } else { ((map.len() as isize) + remainder.1) as usize },
+      );
+      // top
+      if (pos.1 > 0 && map[pos.1-1][pos.0] != '#') || (pos.1 == 0 && map[map.len()-1][pos.0] != '#') {
+        next.insert((s.0, s.1 - 1));
+      }
+      // right
+      if (pos.0 < map[pos.1].len() - 1 && map[pos.1][pos.0+1] != '#') || (pos.0 == map[pos.1].len() - 1 && map[pos.1][0] != '#') {
+        next.insert((s.0 + 1, s.1));
+      }
+      // bottom
+      if (pos.1 < map.len() - 1 && map[pos.1+1][pos.0] != '#') || (pos.1 == map.len() - 1 && map[0][pos.0] != '#') {
+        next.insert((s.0, s.1 + 1));
+      }
+      // left
+      if (pos.0 > 0 && map[pos.1][pos.0-1] != '#') || (pos.0 == 0 && map[pos.1][map[pos.1].len()-1] != '#') {
+        next.insert((s.0 - 1, s.1));
+      }
+    }
+    state = next;
+  }
+
+  state.len()
+}
 
 #[cfg(test)]
 mod tests {
@@ -84,18 +125,60 @@ mod tests {
       assert_eq!(result, 3642);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //   let inputs = read_file("./src/test1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 167409079868000);
-    // }
+    #[test]
+    fn part2_case1() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 6);
+      assert_eq!(result, 16);
+    }
+
+    #[test]
+    fn part2_case2() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 10);
+      assert_eq!(result, 50);
+    }
+
+    #[test]
+    fn part2_case3() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 50);
+      assert_eq!(result, 1594);
+    }
+
+    #[test]
+    fn part2_case4() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 100);
+      assert_eq!(result, 6536);
+    }
+
+    #[test]
+    fn part2_case5() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 500);
+      assert_eq!(result, 167004);
+    }
+
+    #[test]
+    fn part2_case6() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 1000);
+      assert_eq!(result, 668697);
+    }
+
+    #[test]
+    fn part2_case7() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs, 5000);
+      assert_eq!(result, 16733044);
+    }
 
     // #[test]
     // fn part2() {
     //   let inputs = read_file("./src/input1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 8754);
+    //   let result = solve_part2(&inputs, 26501365);
+    //   assert_eq!(result, 0);
     // }
 
     fn read_file(file_path: &str) -> Vec<String> {
