d610059470fd1a439560f85ebbc94ea8ae493796
koooge <koooooge@gmail.com>
2023-12-24 21:57:12 +0100

    2023/day24: ⭐❓
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2023/day24/src/lib.rs
+++ 2023/day24/src/lib.rs
@@ -58,6 +58,43 @@ pub fn solve_part1(inputs: &[String], least: isize, most: isize) -> usize {
   ret
 }
 
+pub fn solve_part2(inputs: &[String]) -> isize {
+  let mut ret = Hail {
+    pos: Pos(0, 0, 0),
+    velocity: Velocity(0, 0, 0),
+    a: 0_f64,
+    b: 0_f64,
+  };
+  let mut hails: Vec<Hail> = vec![];
+  for input in inputs {
+    let (p, v) = input.split_once(" @ ").unwrap();
+    let pos: Vec<isize> = p.split(',').filter_map(|x| x.trim().parse().ok()).collect();
+    let velocity: Vec<isize> = v.split(',').filter_map(|x| x.trim().parse().ok()).collect();
+    let a = velocity[1] as f64 / velocity[0] as f64;
+    let hail = Hail {
+      pos: Pos(pos[0], pos[1], pos[2]),
+      velocity: Velocity(velocity[0], velocity[1], velocity[2]),
+      a: a,
+      b: pos[1] as f64 - (a * pos[0] as f64),
+    };
+    hails.push(hail);
+  }
+
+  for i in 0..hails.len()-1 {
+    for j in i+1..hails.len() {
+      let (hail_a, hail_b) = (hails.get(i).unwrap(), hails.get(j).unwrap());
+      let x = (hail_b.b - hail_a.b) / (hail_a.a - hail_b.a);
+      if !hail_a.is_future(x) || !hail_b.is_future(x) {
+        continue;
+      }
+
+      let y = hail_a.dx(x);
+    }
+  }
+
+  ret.pos.0 + ret.pos.1 + ret.pos.2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -77,12 +114,12 @@ mod tests {
       assert_eq!(result, 25433);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //   let inputs = read_file("./src/test1.txt");
-    //   let result = solve_part2(&inputs);
-    //   assert_eq!(result, 154);
-    // }
+    #[test]
+    fn part2_case1() {
+      let inputs = read_file("./src/test1.txt");
+      let result = solve_part2(&inputs);
+      assert_eq!(result, 47);
+    }
 
     // #[test]
     // fn part2() {
