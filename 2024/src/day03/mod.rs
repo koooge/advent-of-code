af667c305fb5c77cca2607427e05fdff0735ef84
koooge <koooooge@gmail.com>
2024-12-03 21:49:13 +0100

    2024/day03: ⭐
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 2024/src/day03/mod.rs
@@ -0,0 +1,62 @@
+pub fn solve_part1(inputs: &[String]) -> isize {
+    let mut ret = 0;
+
+    for input in inputs {
+        let mut i = 0;
+        while i < input.len() - 8 {
+            if &input[i..=i + 3] == "mul(" {
+                i += 4;
+                if let Some(j) = input[i..].find(')') {
+                    let xy: Result<Vec<isize>, _> = input[i..i + j]
+                        .split(',')
+                        .map(|n| n.parse::<isize>())
+                        .collect();
+                    if let Ok(xy) = xy {
+                        if xy.len() == 2 && xy[0] > 0 && xy[0] <= 999 && xy[1] > 0 && xy[1] <= 999 {
+                            ret += xy[0] * xy[1];
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+            i += 1;
+        }
+    }
+
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::read_file;
+
+    #[test]
+    fn part1_case1() {
+        let inputs = read_file("./src/day03/test1.txt");
+        let result = solve_part1(&inputs);
+        assert_eq!(result, 161);
+    }
+
+    #[test]
+    fn part1() {
+        let inputs = read_file("./src/day03/input1.txt");
+        let result = solve_part1(&inputs);
+        assert_eq!(result, 170068701);
+    }
+
+    // #[test]
+    // fn part2_case1() {
+    //     let inputs = read_file("./src/day03/test1.txt");
+    //     let result = solve_part2(&inputs);
+    //     assert_eq!(result, 4);
+    // }
+
+    // #[test]
+    // fn part2() {
+    //     let inputs = read_file("./src/day03/input1.txt");
+    //     let result = solve_part2(&inputs);
+    //     assert_eq!(result, 577);
+    // }
+}
