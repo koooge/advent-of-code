124ce12f67dd3a9f0cd50e0da73c8c7b84d9d56e
koooge <koooooge@gmail.com>
2024-12-08 15:58:14 +0100

    2024/day08: ⭐⭐
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2024/src/day08/mod.rs
+++ 2024/src/day08/mod.rs
@@ -43,6 +43,63 @@ pub fn solve_part1(inputs: &[String]) -> usize {
     freqs.len()
 }
 
+pub fn solve_part2(inputs: &[String]) -> usize {
+    let mut antenna: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+
+    for (i, input) in inputs.iter().enumerate() {
+        for (j, c) in input.chars().enumerate() {
+            if c == '.' {
+                continue;
+            }
+            antenna.entry(c).or_default().push((i, j));
+        }
+    }
+
+    fn get_antinodes(
+        x_len: usize,
+        y_len: usize,
+        first: (usize, usize),
+        second: (usize, usize),
+    ) -> HashSet<(usize, usize)> {
+        let mut ret = HashSet::new();
+        let a = (second.1 as isize - first.1 as isize) as f64
+            / (second.0 as isize - first.0 as isize) as f64;
+        let b: f64 = first.1 as f64 - (a * first.0 as f64);
+        let dx = second
+            .0
+            .checked_sub(first.0)
+            .unwrap_or_else(|| first.0 - second.0);
+        // left
+        for i in (0..=first.0).rev().step_by(dx) {
+            let y = a * i as f64 + b;
+            if y >= 0.0 && y < y_len as f64 {
+                ret.insert((i, y.round() as usize));
+            }
+        }
+        // right
+        for i in (second.0..x_len).step_by(dx) {
+            let y = a * i as f64 + b;
+            if y >= 0.0 && y < y_len as f64 {
+                ret.insert((i, y.round() as usize));
+            }
+        }
+
+        ret
+    }
+
+    let mut freqs: HashSet<(usize, usize)> = HashSet::new();
+    for values in antenna.values() {
+        for (i, &a) in values.iter().enumerate() {
+            for &b in values.iter().skip(i + 1) {
+                let anti_nodes = get_antinodes(inputs[0].len(), inputs.len(), a, b);
+                freqs.extend(anti_nodes);
+            }
+        }
+    }
+
+    freqs.len()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,17 +119,24 @@ mod tests {
         assert_eq!(result, 329);
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/day08/test1.txt");
-    //     let result = solve_part2(&inputs);
-    //     assert_eq!(result, 6);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/day08/test2.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 9);
+    }
 
-    // #[test]
-    // fn part2() {
-    //     let inputs = read_file("./src/day08/input1.txt");
-    //     let result = solve_part2(&inputs);
-    //     assert_eq!(result, 1);
-    // }
+    #[test]
+    fn part2_case2() {
+        let inputs = read_file("./src/day08/test1.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 34);
+    }
+
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/day08/input1.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 1190);
+    }
 }
