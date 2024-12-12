9468221e1f4719c6dcaaed479a6f7b38506d99be
koooge <koooooge@gmail.com>
2024-12-12 19:41:38 +0100

    2024/day12: ⭐TODO
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2024/src/day12/mod.rs
+++ 2024/src/day12/mod.rs
@@ -1,3 +1,35 @@
+fn get_region(
+    map: &[Vec<char>],
+    checked: &[Vec<bool>],
+    region: &[(usize, usize)],
+    pos: (usize, usize),
+) -> (Vec<(usize, usize)>, Vec<Vec<bool>>) {
+    let mut r = region.to_vec();
+    r.push((pos.0, pos.1));
+    let mut ch = checked.to_vec();
+    ch[pos.0][pos.1] = true;
+
+    let c = map[pos.0][pos.1];
+    // top
+    if pos.0 > 0 && !ch[pos.0 - 1][pos.1] && c == map[pos.0 - 1][pos.1] {
+        (r, ch) = get_region(map, &ch, &r, (pos.0 - 1, pos.1));
+    }
+    // right
+    if pos.1 < map[pos.0].len() - 1 && !ch[pos.0][pos.1 + 1] && c == map[pos.0][pos.1 + 1] {
+        (r, ch) = get_region(map, &ch, &r, (pos.0, pos.1 + 1));
+    }
+    // bottom
+    if pos.0 < map.len() - 1 && !ch[pos.0 + 1][pos.1] && c == map[pos.0 + 1][pos.1] {
+        (r, ch) = get_region(map, &ch, &r, (pos.0 + 1, pos.1));
+    }
+    // left
+    if pos.1 > 0 && !ch[pos.0][pos.1 - 1] && c == map[pos.0][pos.1 - 1] {
+        (r, ch) = get_region(map, &ch, &r, (pos.0, pos.1 - 1));
+    }
+
+    (r, ch)
+}
+
 pub fn solve_part1(inputs: &[String]) -> usize {
     let mut map: Vec<Vec<char>> = vec![];
     let mut checked: Vec<Vec<bool>> = vec![];
@@ -5,38 +37,6 @@ pub fn solve_part1(inputs: &[String]) -> usize {
         map.push(input.chars().collect());
         let c: Vec<bool> = vec![false; input.len()];
         checked.push(c);
-    }
-
-    fn get_region(
-        map: &[Vec<char>],
-        checked: &[Vec<bool>],
-        region: &[(usize, usize)],
-        pos: (usize, usize),
-    ) -> (Vec<(usize, usize)>, Vec<Vec<bool>>) {
-        let mut r = region.to_vec();
-        r.push((pos.0, pos.1));
-        let mut ch = checked.to_vec();
-        ch[pos.0][pos.1] = true;
-
-        let c = map[pos.0][pos.1];
-        // top
-        if pos.0 > 0 && !ch[pos.0 - 1][pos.1] && c == map[pos.0 - 1][pos.1] {
-            (r, ch) = get_region(map, &ch, &r, (pos.0 - 1, pos.1));
-        }
-        // right
-        if pos.1 < map[pos.0].len() - 1 && !ch[pos.0][pos.1 + 1] && c == map[pos.0][pos.1 + 1] {
-            (r, ch) = get_region(map, &ch, &r, (pos.0, pos.1 + 1));
-        }
-        // bottom
-        if pos.0 < map.len() - 1 && !ch[pos.0 + 1][pos.1] && c == map[pos.0 + 1][pos.1] {
-            (r, ch) = get_region(map, &ch, &r, (pos.0 + 1, pos.1));
-        }
-        // left
-        if pos.1 > 0 && !ch[pos.0][pos.1 - 1] && c == map[pos.0][pos.1 - 1] {
-            (r, ch) = get_region(map, &ch, &r, (pos.0, pos.1 - 1));
-        }
-
-        (r, ch)
     }
 
     fn get_perimeter(region: &[(usize, usize)]) -> usize {
@@ -70,6 +70,42 @@ pub fn solve_part1(inputs: &[String]) -> usize {
             (region, checked) = get_region(&map, &checked, &region, (i, j));
             let perimeter = get_perimeter(&region);
             prices.push(region.len() * perimeter);
+        }
+    }
+
+    prices.iter().sum()
+}
+
+pub fn solve_part2(inputs: &[String]) -> usize {
+    let mut map: Vec<Vec<char>> = vec![];
+    let mut checked: Vec<Vec<bool>> = vec![];
+    for input in inputs {
+        map.push(input.chars().collect());
+        let c: Vec<bool> = vec![false; input.len()];
+        checked.push(c);
+    }
+
+    fn get_sides(region: &[(usize, usize)]) -> usize {
+        let mut ret: usize = 0;
+
+        // TODO: here
+        for pos in region {
+            println!("{:?}", pos);
+            ret += 1;
+        }
+        ret
+    }
+
+    let mut prices: Vec<usize> = vec![];
+    for i in 0..checked.len() {
+        for j in 0..checked[i].len() {
+            if checked[i][j] {
+                continue;
+            }
+            let mut region = vec![];
+            (region, checked) = get_region(&map, &checked, &region, (i, j));
+            let sides = get_sides(&region);
+            prices.push(region.len() * sides);
         }
     }
 
@@ -113,7 +149,35 @@ mod tests {
     // fn part2_case1() {
     //     let inputs = read_file("./src/day12/test1.txt");
     //     let result = solve_part2(&inputs);
-    //     assert_eq!(result, 65601038650482);
+    //     assert_eq!(result, 80);
+    // }
+
+    // #[test]
+    // fn part2_case2() {
+    //     let inputs = read_file("./src/day12/test2.txt");
+    //     let result = solve_part1(&inputs);
+    //     assert_eq!(result, 436);
+    // }
+
+    // #[test]
+    // fn part2_case3() {
+    //     let inputs = read_file("./src/day12/test4.txt");
+    //     let result = solve_part1(&inputs);
+    //     assert_eq!(result, 236);
+    // }
+
+    // #[test]
+    // fn part2_case4() {
+    //     let inputs = read_file("./src/day12/test5.txt");
+    //     let result = solve_part1(&inputs);
+    //     assert_eq!(result, 368);
+    // }
+
+    // #[test]
+    // fn part2_case5() {
+    //     let inputs = read_file("./src/day12/test3.txt");
+    //     let result = solve_part1(&inputs);
+    //     assert_eq!(result, 1206);
     // }
 
     // #[test]
