31a63e73ea27d7d922d7facce2dbbba1eacb02e7
koooge <koooooge@gmail.com>
2024-12-19 00:48:45 +0100

    2024/day17: ⭐🐢
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2024/src/day17/mod.rs
+++ 2024/src/day17/mod.rs
@@ -1,16 +1,5 @@
-pub fn solve_part1(inputs: &[String]) -> String {
-    let mut regs: Vec<usize> = vec![0; 3];
-    regs[0] = inputs[0].split(": ").last().unwrap().parse().unwrap();
-    regs[1] = inputs[1].split(": ").last().unwrap().parse().unwrap();
-    regs[2] = inputs[2].split(": ").last().unwrap().parse().unwrap();
-    let program: Vec<usize> = inputs[4]
-        .split(": ")
-        .last()
-        .unwrap()
-        .split(",")
-        .filter_map(|c| c.parse().ok())
-        .collect();
-
+fn solve(registers: &[usize], program: &[usize]) -> String {
+    let mut regs = registers.to_vec();
     let mut counter: usize = 0;
     let mut ret: Vec<usize> = vec![];
     while let Some(opcode) = program.get(counter) {
@@ -50,9 +39,37 @@ pub fn solve_part1(inputs: &[String]) -> String {
         .join(",")
 }
 
-// pub fn solve_part2(inputs: &[String]) -> usize {
-//   0
-// }
+pub fn solve_part1(inputs: &[String]) -> String {
+    let mut regs: Vec<usize> = vec![0; 3];
+    regs[0] = inputs[0].split(": ").last().unwrap().parse().unwrap();
+    regs[1] = inputs[1].split(": ").last().unwrap().parse().unwrap();
+    regs[2] = inputs[2].split(": ").last().unwrap().parse().unwrap();
+    let program: Vec<usize> = inputs[4]
+        .split(": ")
+        .last()
+        .unwrap()
+        .split(",")
+        .filter_map(|c| c.parse().ok())
+        .collect();
+    solve(&regs, &program)
+}
+
+pub fn solve_part2(inputs: &[String]) -> usize {
+    let regs_b: usize = inputs[1].split(": ").last().unwrap().parse().unwrap();
+    let regs_c: usize = inputs[2].split(": ").last().unwrap().parse().unwrap();
+    let output = inputs[4].split(": ").last().unwrap();
+    let program: Vec<usize> = output.split(",").filter_map(|c| c.parse().ok()).collect();
+
+    let mut ret = 0;
+    loop {
+        let regs = vec![ret, regs_b, regs_c];
+        let o = solve(&regs, &program);
+        if output == o {
+            return ret;
+        }
+        ret += 1;
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -73,12 +90,12 @@ mod tests {
         assert_eq!(result, String::from("6,4,6,0,4,5,7,2,7"));
     }
 
-    // #[test]
-    // fn part2_case1() {
-    //     let inputs = read_file("./src/day17/test1.txt");
-    //     let result = solve_part2(&inputs);
-    //     assert_eq!(result, 45);
-    // }
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/day17/test2.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 117440);
+    }
 
     // #[test]
     // fn part2() {
