5643ed9e2eee868585e159a536032bdb4fc153bd
koooge <koooooge@gmail.com>
2025-12-03 03:32:28 +0900

    2025/day02: **
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2025/src/day02/mod.rs
+++ 2025/src/day02/mod.rs
@@ -33,9 +33,55 @@ pub fn solve_part1(inputs: &[String]) -> usize {
     ret
 }
 
-// pub fn solve_part2(inputs: &[String], p: usize) -> usize {
+fn is_invalid2(n: usize) -> bool {
+    if n < 10 {
+        return false;
+    }
+    let s = n.to_string();
+    let len = s.len();
+    if s.chars().min() == s.chars().max() {
+        return true;
+    }
 
-// }
+    for i in 1..=(len / 2) {
+        if !len.is_multiple_of(i) {
+            continue;
+        }
+        let first = &s[0..i];
+        if s.as_bytes()
+            .chunks(i)
+            .all(|chunk| chunk == first.as_bytes())
+        {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn solve_part2(inputs: &[String]) -> usize {
+    let mut ret = 0;
+
+    for line in inputs {
+        for r in line.split(',') {
+            if r.is_empty() {
+                continue;
+            }
+
+            let (first, last): (usize, usize) = r
+                .split_once('-')
+                .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
+                .unwrap();
+
+            for n in first..=last {
+                if is_invalid2(n) {
+                    ret += n;
+                }
+            }
+        }
+    }
+
+    ret
+}
 
 #[cfg(test)]
 mod tests {
@@ -54,5 +100,40 @@ mod tests {
         let inputs = read_file("./src/day02/input1.txt");
         let result = solve_part1(&inputs);
         assert_eq!(result, 56660955519);
+    }
+
+    #[test]
+    fn day01_part2_case1() {
+        let inputs = read_file("./src/day02/test1.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 4174379265);
+    }
+
+    #[test]
+    fn day01_part2_case2() {
+        let inputs = vec![String::from("11-22")];
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 33);
+    }
+
+    #[test]
+    fn day01_part2_case3() {
+        let inputs = vec![String::from("1009-1011")];
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 1010);
+    }
+
+    #[test]
+    fn day01_part2_case4() {
+        let inputs = vec![String::from("1-10")];
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn part2() {
+        let inputs = read_file("./src/day02/input1.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 79183223243);
     }
 }
