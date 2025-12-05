3e692f31a077954b1ac75ba887e72501b6541f20
koooge <koooooge@gmail.com>
2025-12-05 16:33:39 +0900

    2025/day05: *
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 2025/src/day05/mod.rs
@@ -0,0 +1,51 @@
+pub fn solve_part1(inputs: &[String]) -> usize {
+    let mut ret = 0;
+    let mut fresh_ranges: Vec<(usize, usize)> = vec![];
+
+    for line in inputs {
+        if line.is_empty() {
+            continue;
+        }
+        if line.contains('-') {
+            let (a, b) = line
+                .split_once('-')
+                .map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap()))
+                .unwrap();
+            fresh_ranges.push((a, b));
+            continue;
+        }
+
+        let n: usize = line.parse().unwrap();
+        for (begin, end) in &fresh_ranges {
+            if n >= *begin && n <= *end {
+                ret += 1;
+                break;
+            }
+        }
+    }
+
+    ret
+}
+
+// pub fn solve_part2(inputs: &[String]) -> usize {
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::read_file;
+
+    #[test]
+    fn day05_part1_case1() {
+        let inputs = read_file("./src/day05/test1.txt");
+        let result = solve_part1(&inputs);
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn part1() {
+        let inputs = read_file("./src/day05/input1.txt");
+        let result = solve_part1(&inputs);
+        assert_eq!(result, 821);
+    }
+}
