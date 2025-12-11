309404ec7f34a17a50c75ae50ea190805ed68f21
koooge <koooooge@gmail.com>
2025-12-12 01:15:06 +0900

    2025/day10: *s
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2025/src/day10/mod.rs
+++ 2025/src/day10/mod.rs
@@ -48,8 +48,64 @@ pub fn solve_part1(inputs: &[String]) -> usize {
     ret
 }
 
-// pub fn solve_part2(inputs: &[String]) -> usize {
-// }
+fn push_button(cur: &[usize], button: &[usize]) -> Vec<usize> {
+    let mut ret = cur.to_vec();
+    for &n in button {
+        ret[n] += 1;
+    }
+    ret
+}
+
+fn min_push_2(requirements: &[usize], buttons: &[Vec<usize>]) -> usize {
+    let mut states = vec![vec![0; requirements.len()]];
+    let mut next_states = vec![];
+    for i in 1.. {
+        while let Some(cur) = states.pop() {
+            for button in buttons {
+                let next = push_button(&cur, button);
+                if next == requirements {
+                    return i;
+                } else if next.iter().zip(requirements.iter()).all(|(a, b)| a <= b) {
+                    next_states.push(next);
+                }
+            }
+        }
+        states = next_states;
+        next_states = vec![];
+    }
+    unreachable!()
+}
+
+pub fn solve_part2(inputs: &[String]) -> usize {
+    let mut ret = 0;
+
+    for line in inputs {
+        let (_, b) = line.split_once(' ').unwrap();
+        let (s, c) = b.split_once('{').unwrap();
+        let buttons: Vec<Vec<usize>> = s
+            .split_whitespace()
+            .map(|group| {
+                group
+                    .trim_matches(|c| c == '(' || c == ')')
+                    .split(',')
+                    .filter(|x| !x.is_empty())
+                    .map(|x| x.parse::<usize>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+        let requirements: Vec<usize> = c
+            .split(',')
+            .map(|s| {
+                s.trim_matches(|c: char| c == '}' || c.is_whitespace())
+                    .parse::<usize>()
+                    .unwrap()
+            })
+            .collect();
+        ret += min_push_2(&requirements, &buttons);
+    }
+
+    ret
+}
 
 #[cfg(test)]
 mod tests {
@@ -69,4 +125,18 @@ mod tests {
         let result = solve_part1(&inputs);
         assert_eq!(result, 434);
     }
+
+    #[test]
+    fn part2_case1() {
+        let inputs = read_file("./src/day10/test1.txt");
+        let result = solve_part2(&inputs);
+        assert_eq!(result, 33);
+    }
+
+    // #[test]
+    // fn part2() {
+    //     let inputs = read_file("./src/day10/input1.txt");
+    //     let result = solve_part2(&inputs);
+    //     assert_eq!(result, 434);
+    // }
 }
